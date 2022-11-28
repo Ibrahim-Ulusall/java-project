c98cd3e29d0efeab861c613dc6566a0a6a5af811
İbrahim Ulusal <94749717+Ibrahim-Ulusall@users.noreply.github.com>
2022-11-29 02:09:22 +0300

    Update palindromikKelimeler.java

--- Project-0/src/palindromikKelimeler.java
+++ Project-0/src/palindromikKelimeler.java
@@ -6,17 +6,17 @@ public class palindromikKelimeler{
 		System.out.print("Bir kelime Giriniz : ");
 		String text = input.nextLine();
 		String new_text = "";
-		char[] wordList = new char[text.length()];
-		for(int i=0; i< wordList.length; i++) wordList[i] =  text.charAt(i);
+		char[] charList = new char[text.length()];
+		for(int i=0; i< charList.length; i++) charList[i] =  text.charAt(i);
 
 		char reverse;
-		for(int k=0; k<wordList.length /  2; k++) {
-			reverse = wordList[k];
-			wordList[k] = wordList[wordList.length - k -1];
-			wordList[wordList.length - k -1] = reverse;		
+		for(int k=0; k<charList.length /  2; k++) {
+			reverse = charList[k];
+			charList[k] = charList[charList.length - k -1];
+			charList[charList.length - k -1] = reverse;		
 		}
 
-		for(char o:wordList) new_text+=o;
+		for(char o:charList) new_text+=o;
 		if(text.equals(new_text)) System.out.print("\n" + text +" it is a palindromic word");
 		else System.out.print("\n" + text + " it is not a palindromic word.");
 		
